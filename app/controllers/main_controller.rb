144e22eab9a3cd2a27a3f4ea7c554331445490e5
witZack <wcz8292@gmail.com>
2016-03-02 22:31:10 -0800

    Initial html for profile page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,47 @@
 class MainController < ApplicationController
   # Main Builder Page
   def index()
+    @experiences = experiences
+    @skills = skills
+    @about = about
+    @interests = interests
+    @profile_image = profile_image
+  end
+
+  private
+  # This section is where you input your information
+  def about()
+    "This is some text about me."
+  end
+  def experiences()
+    [
+      Experience.new("MyFitnessPal",
+        "http://myfitnesspal.com",
+        "Senior Software Engineer",
+        ["Magician"],
+        DateTime.new(2014,1,10)),
+      Experience.new("MyFitnessPal",
+        "http://myfitnesspal.com",
+        "Software Engineer",
+        ["Magician"],
+        DateTime.new(2012,1,10),
+        DateTime.new(2013,1,10))
+    ]
+  end
+  def skills()
+    [
+      Skill.new("Languages",
+        ["Magician"]),
+      Skill.new("API",
+        ["Design"])
+    ]
+  end
+  def interests()
+    [
+
+    ]
+  end
+  def profile_image()
+    "assets/images/profile.jpg"
   end
 end
