6da60b02f529721e7f494971448d1a15d978b1fb
Whitney Zack <wcz8292@gmail.com>
2016-03-06 15:45:54 -0800

    Revert "Remove unused database dependency for now."
    This reverts commit b13946d04f600e35ef595e0655969213ea50ba0d.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  # config.active_record.migration_error = :page_load
+  config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
@@ -40,5 +40,5 @@ Rails.application.configure do
   config.web_console.whiny_requests = false
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  
 end
